fba23c32e1158d72d87e7301d570be317633f888
Julius Oyovwikigho <julius.oyovwikigho@garrison.green>
2023-04-25 22:25:22 +0100

    route added

---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,5 @@
+function Login(): JSX.Element {
+  return <div>Please login</div>;
+}
+
+export default Login;
---
+++ src/pages/signup/SignUp.tsx
@@ -0,0 +1,5 @@
+function SignUp(): JSX.Element {
+  return <div>Please sign up here</div>;
+}
+
+export default SignUp;
